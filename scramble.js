1e591eb7248d99d491be16b1c64e2394e5cecdc9
Max Cohn <mxc1805@rit.edu>
2019-10-05 12:25:41 -0400

    Fixed working with u/

--- scramble.js
+++ scramble.js
@@ -1,4 +1,5 @@
 
+// Generates random name
 function makeid(length) {
     var result = '';
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -9,7 +10,7 @@ function makeid(length) {
     return result;
 }
 
-// generate a ranodm username to replace the actual username
+// generate a random username to replace the actual username
 newName = makeid(10);
 
 // get user name from this given point. If Reddit UI changes, this will have to
@@ -18,15 +19,15 @@ let el = document.querySelector('.header-user-dropdown span span span span');
 userName = el.textContent;
 el.textContent = newName;
 
-
-// main loop
 // every 5 seconds, go through all `a` tags and replace their text if it is the
 // original username
 setInterval(function(){
-    document.querySelectorAll('a').forEach(n => n.textContent = (n.textContent.includes(userName) ? newName : n.textContent));
+    // for each `a` tag, check if it contains the username
+    document.querySelectorAll('a').forEach(n => {
+        // if it does, replace it with the new name
+        if(n.textContent.includes(userName)){
+            n.textContent = (n.textContent.includes('u/') ? 'u/' : '') + newName;
+        }
+    });    
 }, 5000);
 
-//document.querySelectorAll('a').forEach(n => n.textContent = 'ASDASD');
-//const delay = ms => new Promise(res => setTimeout(res, ms));
-//await delay(7000);
-//console.log('asdasdasdasd');
